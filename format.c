ee6e4c6d2b0accd149f5606b5bc99d8f0d88a135
Ivan-Djientcheu <ivan.djientcheu-tchamedeu.stud.thga.de>
2023-05-08 05:41:51 +0200

    neues Programm format.c

---
+++ format.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{   int Int = 3001;
+    float myFloat = 3.14;
+    char myChar = 'a';
+    char myString[] = "Hello world!";
+
+    printf("Integer: %d\n", Int);
+    printf("Float: %f\n", myFloat);
+    printf("Character: %c\n", myChar);
+    printf("String: %s\n", myString);
+
+}
+
